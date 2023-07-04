729b3d5a9b1b534d87a26d329d582dff301f631e
Absolpega <106615943+Absolpega@users.noreply.github.com>
2023-07-04 23:58:23 +0200

    i rewrote the whole thing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::os::unix::io::AsRawFd;
+use clap::Parser;
 
-use std::array::IntoIter;
-use std::iter::Cycle;
-
-use std::fs::File;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = false)]
+    secure: bool,
+}
 
 use termios::*;
-
-use std::io::{Write, Read, stdin, stdout};
 
 use ansi_escapes;
 
@@ -16,21 +16,189 @@ use colorful::Colorful;
 
 use read_char::read_next_char;
 
-fn end(orig_termios: Termios) {
-    tcsetattr(stdin().as_raw_fd(), TCSAFLUSH, &orig_termios).unwrap();
+use std::fs::File;
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
+use std::iter::Cycle;
+use std::os::fd::AsRawFd;
+
+struct TermiosWrapper {
+    orig_termios: Termios,
+    termios: Termios,
 }
 
-fn termios_init() -> Termios {
-    let mut termios: Termios = Termios::from_fd(stdin().as_raw_fd()).unwrap();
-    tcgetattr(stdin().as_raw_fd(), &mut termios).unwrap();
-    return termios;
+impl TermiosWrapper {
+    fn new() -> Self {
+        let mut termios: Termios = Termios::from_fd(stdin().as_raw_fd()).unwrap();
+        tcgetattr(stdin().as_raw_fd(), &mut termios).unwrap();
+        return Self {
+            orig_termios: termios.clone(),
+            termios,
+        };
+    }
+    fn raw(&mut self) {
+        tcgetattr(stdin().as_raw_fd(), &mut self.termios).unwrap();
+        self.termios.c_lflag &= !(ICRNL | IXON);
+        self.termios.c_lflag &= !(ECHO | ICANON | IEXTEN);
+        tcsetattr(stdin().as_raw_fd(), TCSAFLUSH, &self.termios).unwrap();
+    }
 }
 
-fn raw(raw: &mut Termios) {
-    tcgetattr(stdin().as_raw_fd(), raw).unwrap();
-	raw.c_lflag &= !(ICRNL | IXON);
-	raw.c_lflag &= !(ECHO | ICANON | IEXTEN);
-    tcsetattr(stdin().as_raw_fd(), TCSAFLUSH, &raw).unwrap();
+impl Drop for TermiosWrapper {
+    fn drop(&mut self) {
+        tcsetattr(stdin().as_raw_fd(), TCSAFLUSH, &self.orig_termios).unwrap();
+    }
+}
+
+#[derive(PartialEq)]
+enum SpinType {
+    Empty,
+    Forward,
+    Backward,
+    Secure,
+}
+
+fn spin<T>(way: SpinType, tty: &mut File, iter: &mut Cycle<T>, password: &String, spinner: &Spinner)
+where
+    T: Iterator<Item = char>,
+    T: Clone,
+{
+    let mut offset = spinner.offset + password.chars().count();
+
+    if way == SpinType::Secure {
+        offset = spinner.offset;
+    }
+
+    if way == SpinType::Backward {
+        for _ in 1..spinner.characters.len() - 1 {
+            iter.next();
+        }
+    }
+
+    let some_icon: Option<char> = match way {
+        SpinType::Secure => Some(spinner.secure),
+        SpinType::Empty => Some(spinner.empty),
+        _ => None,
+    };
+
+    write!(tty, "{}", ansi_escapes::CursorHide).unwrap();
+    write!(tty, "{}", ansi_escapes::CursorSavePosition).unwrap();
+    write!(
+        tty,
+        "{}",
+        ansi_escapes::CursorBackward(offset.try_into().unwrap())
+    )
+    .unwrap();
+
+    write!(
+        tty,
+        "{}",
+        some_icon
+            .unwrap_or_else(|| iter.next().unwrap())
+            .to_string()
+            .cyan()
+    )
+    .unwrap();
+
+    write!(tty, "{}", ansi_escapes::CursorRestorePosition).unwrap();
+    write!(tty, "{}", ansi_escapes::CursorShow).unwrap();
+}
+
+fn main() {
+    let mut tty = File::create("/dev/tty").unwrap();
+
+    let args = Args::parse();
+
+    let mut termios_wrapper = TermiosWrapper::new();
+    termios_wrapper.raw();
+
+    let mut password: String = String::new();
+
+    let spinner = Spinner {
+        characters: vec!['1', '2', '3', '4'],
+        empty: 'e',
+        secure: 'V',
+        offset: 4,
+    };
+
+    let mut spinner_iter = spinner.characters.clone().into_iter().cycle();
+
+    write!(tty, "{}", String::from("Enter password < S > ").yellow()).unwrap();
+
+    spin(
+        SpinType::Empty,
+        &mut tty,
+        &mut spinner_iter,
+        &password,
+        &spinner,
+    );
+
+    loop {
+        match read_next_char(&mut stdin()).unwrap() {
+            '\n' => break,
+            '\x7F' => {
+                // backspace
+                if password.pop().is_some() && !args.secure {
+                    write!(tty, "{}", ansi_escapes::CursorBackward(1)).unwrap();
+                    write!(tty, " ").unwrap();
+                    write!(tty, "{}", ansi_escapes::CursorBackward(1)).unwrap();
+
+                    spin(
+                        SpinType::Backward,
+                        &mut tty,
+                        &mut spinner_iter,
+                        &password,
+                        &spinner,
+                    );
+                }
+
+                if password.chars().last().is_none() {
+                    spin(
+                        SpinType::Empty,
+                        &mut tty,
+                        &mut spinner_iter,
+                        &password,
+                        &spinner,
+                    );
+                }
+            }
+            character => {
+                password.push(character);
+
+                if !args.secure {
+                    write!(tty, "*").unwrap();
+
+                    spin(
+                        SpinType::Forward,
+                        &mut tty,
+                        &mut spinner_iter,
+                        &password,
+                        &spinner,
+                    );
+                } else {
+                    spin(
+                        SpinType::Secure,
+                        &mut tty,
+                        &mut spinner_iter,
+                        &password,
+                        &spinner,
+                    );
+                }
+            }
+        }
+    }
+
+    write!(tty, "\n").unwrap();
+
+    write!(stdout(), "{}\n", password).unwrap();
+}
+
+struct Spinner {
+    characters: Vec<char>,
+    empty: char,
+    secure: char,
+    offset: usize,
 }
 
 //const SPINNER_CHARACTERS: [&str; SPINNER_CHARACTERS_SIZE] = [
@@ -44,171 +212,29 @@ fn raw(raw: &mut Termios) {
 //    "󰪥"
 //];
 
-const SPINNER_CHARACTERS: [&str; 24] = [
-    "", // 6
-    "", // 5
-    "", // 4
-    "", // 3
-    "", // 2
-    "", // 1
-    "", // 6
-    "", // 5
-    "", // 4
-    "", // 3
-    "", // 2
-    "", // 1
-    "", // 6
-    "", // 5
-    "", // 4
-    "", // 3
-    "", // 2
-    "", // 1
-    "", // 6
-    "", // 5
-    "", // 4
-    "", // 3
-    "", // 2
-    "", // 1
-];
-
-struct Spinner<'a> {
-     characters: Vec<&'a str>,
-     empty: &'a str,
-     secure: &'a str,
-     offset: usize,
-}
-
-#[derive(PartialEq)]
-enum SpinType {
-    Empty,
-    Forward,
-    Backward,
-    Secure,
-}
-
-fn spin<'a, T>(
-    way: SpinType,
-    tty: &mut File,
-    iter: &mut Cycle<T>,
-    output: &Vec<String>,
-    spinner: &Spinner,
-    ) where T: Clone, T: Iterator<Item = &'a str> {
-
-    //let mut left_offset = (spinner.offset + output.chars().collect::<Vec<_>>().len()).try_into().unwrap();
-    let mut left_offset = (spinner.offset + output.len()).try_into().unwrap();
-
-    // due to the -1
-    // should just never enter a value that == 1
-    if left_offset == 0 {
-        left_offset = 1;
-    }
-
-    if way == SpinType::Backward {
-        for _ in 1..spinner.characters.len()-1 {
-            iter.next();
-        }
-    }
-
-    let mut next: Option<&str> = iter.next();
-
-    match way {
-        SpinType::Empty => {
-            next = Some(spinner.empty);
-        }
-        SpinType::Secure => {
-            next = Some(spinner.secure);
-        }
-        _ => {}
-    }
-
-    if next.is_some() {
-        write!(tty, "{}", ansi_escapes::CursorHide).unwrap();
-        write!(tty, "{}", ansi_escapes::CursorSavePosition).unwrap();
-        write!(tty, "{}", ansi_escapes::CursorBackward(left_offset)).unwrap();
-
-        write!(tty, "{}", next.unwrap().cyan()).unwrap();
-
-        write!(tty, "{}", ansi_escapes::CursorRestorePosition).unwrap();
-        write!(tty, "{}", ansi_escapes::CursorShow).unwrap();
-    }
-}
-
-fn read(
-    mut tty: File,
-    secure: bool,
-    spinner: &Spinner,
-    ) -> String {
-    let mut output: Vec<String> = vec!();
-
-    let mut spinner_characters_iter = spinner.characters.clone().into_iter().cycle();
-    spin(SpinType::Empty, &mut tty, &mut spinner_characters_iter, &output, spinner);
-
-    loop {
-        //match (stdin().bytes().next().unwrap().unwrap() as char).to_string().as_str() {
-        match read_next_char(&mut stdin()).unwrap().to_string().as_str() {
-            "\n" => break,
-            "\x7F" => {
-                // it returns 127 (7F) on backspace for some reason
-                
-                if !secure && output.pop() != None {
-                    write!(tty, "{}", ansi_escapes::CursorBackward(1)).unwrap();
-                    write!(tty, " ").unwrap();
-                    write!(tty, "{}", ansi_escapes::CursorBackward(1)).unwrap();
-
-                    spin(SpinType::Backward, &mut tty, &mut spinner_characters_iter, &output, spinner);
-                }
-
-                if output.last() == None {
-                    spin(SpinType::Empty, &mut tty, &mut spinner_characters_iter, &output, spinner);
-                }
-
-            }
-            character => {
-                if !secure {
-                    spin(SpinType::Forward, &mut tty, &mut spinner_characters_iter, &output, spinner);
-                    write!(tty, "*").unwrap();
-                } else {
-                    spin(SpinType::Secure, &mut tty, &mut spinner_characters_iter, &vec!("".to_string()), spinner);
-                }
-                output.push(character.to_string());
-            }
-        }
-    }
-
-    if !secure {
-        spin(SpinType::Empty, &mut tty, &mut spinner_characters_iter, &output, spinner);
-    }
-
-    write!(tty, "\n").unwrap();
-
-    return output.join("");
-}
-
-fn main() {
-    let mut tty = File::create("/dev/tty").unwrap();
-
-    let orig_termios = termios_init();
-
-    let mut termios: Termios = termios_init();
-    raw(&mut termios);
-
-    write!(tty, "{}", String::from("Enter password < S > ").yellow()).unwrap();
-
-    let mut secure: bool = false;
-    if std::env::args().nth(1) == Some("--secure".to_string()) {
-        secure = true;
-    }
-
-    let spinner = Spinner {
-        characters: SPINNER_CHARACTERS.to_vec(),
-        empty: "󱃓",
-        secure: "󰳌",
-        offset: 4,
-    };
-
-    let string = read(tty, secure, &spinner);
-
-    write!(stdout(), "{}\n", string).unwrap();
-
-    end(orig_termios);
-}
+//const SPINNER_CHARACTERS: [&str; 24] = [
+//    "", // 6
+//    "", // 5
+//    "", // 4
+//    "", // 3
+//    "", // 2
+//    "", // 1
+//    "", // 6
+//    "", // 5
+//    "", // 4
+//    "", // 3
+//    "", // 2
+//    "", // 1
+//    "", // 6
+//    "", // 5
+//    "", // 4
+//    "", // 3
+//    "", // 2
+//    "", // 1
+//    "", // 6
+//    "", // 5
+//    "", // 4
+//    "", // 3
+//    "", // 2
+//    "", // 1
+//];
